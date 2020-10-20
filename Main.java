072be45cd490fe64465b403ac6fbdd527d024097
othmanamr <amr.othman@patelhs.org>
2020-10-20 14:50:02 +0000

    .

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello customers !");
-  System.out.println("hello welcome to the bank");
-int money = 70.5;
-double bills = 101;
-boolean cents = 1;
- System.out.println("How much money do you need");
- if (int money == boolean cents);
-  int (today age = boolean birth year age); 
-  
+  System.out.println("hello welcome to birth ticket");
+int birthDay = 06/12;
+double todayDay = 10/19;
+boolean birthYear = 06/12/  `06;
+System.out.println("whats your age");
+  }
+}
